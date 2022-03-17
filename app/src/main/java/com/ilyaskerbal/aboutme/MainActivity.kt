3fb1a08f0ad99f7d4b3e906cef30e45a8fa956aa
ILYAS KERBAL <kerbalsc@gmail.com>
2022-03-17 20:19:46 +0100

    Added a listener to the button

--- app/src/main/java/com/ilyaskerbal/aboutme/MainActivity.kt
+++ app/src/main/java/com/ilyaskerbal/aboutme/MainActivity.kt
@@ -1,11 +1,39 @@
 package com.ilyaskerbal.aboutme
 
+import android.content.Context
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var doneButton : Button
+    lateinit var editText : EditText
+    lateinit var nicknameText : TextView
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+        doneButton = findViewById(R.id.done_button)
+        editText = findViewById(R.id.nickname_field)
+        nicknameText = findViewById(R.id.nickname)
+
+        doneButton.setOnClickListener {
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View) {
+        nicknameText.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nicknameText.visibility = View.VISIBLE
+
+        val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
+        imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
 }
