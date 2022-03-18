119fb966f9d4b60672c469fc8b31968244b991aa
ILYAS KERBAL <kerbalsc@gmail.com>
2022-03-18 11:13:04 +0100

    Adding support for data binding

--- app/src/main/java/com/ilyaskerbal/aboutme/MainActivity.kt
+++ app/src/main/java/com/ilyaskerbal/aboutme/MainActivity.kt
@@ -8,31 +8,31 @@ import android.view.inputmethod.InputMethodManager
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.ilyaskerbal.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var doneButton : Button
-    lateinit var editText : EditText
-    lateinit var nicknameText : TextView
+    private lateinit var binding: ActivityMainBinding
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        doneButton = findViewById(R.id.done_button)
-        editText = findViewById(R.id.nickname_field)
-        nicknameText = findViewById(R.id.nickname)
+        // setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
 
-        doneButton.setOnClickListener {
+        binding.doneButton.setOnClickListener {
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View) {
-        nicknameText.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nicknameText.visibility = View.VISIBLE
-
+        binding.apply {
+            nickname.text = binding.nicknameField.text
+            invalidateAll()
+            view.visibility = View.GONE
+            nicknameField.visibility = View.GONE
+            nickname.visibility = View.VISIBLE
+        }
         val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as InputMethodManager
         imm.hideSoftInputFromWindow(view.windowToken, 0)
     }
